c8e5d9628d61c2643579eeff940d708131a3a4a4
waleed <waleed.p376404@gmail.com>
2026-06-25 15:01:12 +0530

    string without repeating charecter problem with sliding window and hashset

---
+++ strings/string_without_repeating_char.py
@@ -0,0 +1,12 @@
+def repeat(s:str):
+    left=0
+    char_set=set()
+    max_lenght=0
+    for right in range(len(s)):
+        while s[right] in char_set:
+            char_set.remove(s[left])
+            left+=1
+        char_set.add(s[right])
+        max_lenght = max(max_lenght , right - left +1)
+    return max_lenght
+print(repeat("abcabcbb"))
